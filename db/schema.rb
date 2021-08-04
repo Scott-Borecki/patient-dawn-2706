23bb6a54b07f24f2718d96bfaa6b3667a84fafaa
Scott Borecki <scottborecki@gmail.com>
2021-08-04 09:20:25 -0600

    Create Competitions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_162719) do
+ActiveRecord::Schema.define(version: 2021_08_04_151005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "competitions", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "sport"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "name"
