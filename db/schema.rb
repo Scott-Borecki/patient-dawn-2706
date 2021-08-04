adcb75356802c4509a6f112eb2e52a7904f81b2c
Scott Borecki <scottborecki@gmail.com>
2021-08-04 09:41:50 -0600

    Create Participants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_151005) do
+ActiveRecord::Schema.define(version: 2021_08_04_154110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2021_08_04_151005) do
     t.string "sport"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.bigint "team_id"
+    t.bigint "competition_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["competition_id"], name: "index_participants_on_competition_id"
+    t.index ["team_id"], name: "index_participants_on_team_id"
   end
 
   create_table "players", force: :cascade do |t|
@@ -39,5 +48,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_151005) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "participants", "competitions"
+  add_foreign_key "participants", "teams"
   add_foreign_key "players", "teams"
 end
