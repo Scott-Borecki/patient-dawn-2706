26b65264ecc966dc36c39702d8fbd6183b5bbfe2
Scott Borecki <scottborecki@gmail.com>
2021-08-04 10:46:27 -0600

    Add average age to competition show page (Complete User Story 2)

--- spec/features/competitions/index_spec.rb
+++ spec/features/competitions/index_spec.rb
@@ -25,7 +25,48 @@ RSpec.describe 'competitions index page (/competitions)' do
     )
   end
 
+  let!(:team1) { Team.create!(hometown: 'Lessburg', nickname: 'Rockets') }
+  let!(:team2) { Team.create!(hometown: 'Sandwich', nickname: 'Blue Knights') }
+  let!(:team3) { Team.create!(hometown: 'Arvada', nickname: 'Mountaineers') }
+  let!(:team4) { Team.create!(hometown: 'Jefferson', nickname: 'Spectacles') }
+  let!(:team5) { Team.create!(hometown: 'Washington', nickname: 'Cherries') }
+
+  let!(:participant1) {Participant.create!(team: team1, competition: competition1) }
+  let!(:participant2) {Participant.create!(team: team2, competition: competition1) }
+  let!(:participant3) {Participant.create!(team: team3, competition: competition1) }
+  let!(:participant4) {Participant.create!(team: team4, competition: competition2) }
+  let!(:participant5) {Participant.create!(team: team5, competition: competition2) }
+  let!(:participant6) {Participant.create!(team: team1, competition: competition3) }
+  let!(:participant7) {Participant.create!(team: team4, competition: competition3) }
+
+  let!(:player1a) { team1.players.create!(name: 'Player 1a', age: 10) }
+  let!(:player1b) { team1.players.create!(name: 'Player 1b', age: 10) }
+  let!(:player1c) { team1.players.create!(name: 'Player 1c', age: 12) }
+  let!(:player1d) { team1.players.create!(name: 'Player 1d', age: 12) }
+
+  let!(:player2a) { team2.players.create!(name: 'Player 2a', age: 12) }
+  let!(:player2b) { team2.players.create!(name: 'Player 2b', age: 12) }
+  let!(:player2c) { team2.players.create!(name: 'Player 2c', age: 13) }
+  let!(:player2d) { team2.players.create!(name: 'Player 2d', age: 14) }
+  let!(:player2e) { team2.players.create!(name: 'Player 2e', age: 14) }
+
+  let!(:player3a) { team3.players.create!(name: 'Player 3a', age: 10) }
+  let!(:player3b) { team3.players.create!(name: 'Player 3b', age: 12) }
+  let!(:player3c) { team3.players.create!(name: 'Player 3c', age: 14) }
+
+  let!(:player4a) { team4.players.create!(name: 'Player 4a', age: 10) }
+  let!(:player4b) { team4.players.create!(name: 'Player 4b', age: 10) }
+  let!(:player4c) { team4.players.create!(name: 'Player 4c', age: 11) }
+  let!(:player4d) { team4.players.create!(name: 'Player 4d', age: 11) }
+
+  let!(:player5a) { team5.players.create!(name: 'Player 5a', age: 10) }
+  let!(:player5b) { team5.players.create!(name: 'Player 5b', age: 13) }
+  let!(:player5c) { team5.players.create!(name: 'Player 5c', age: 19) }
+
   specify { expect(Competition.all.count.positive?).to be true }
+  specify { expect(Participant.all.count.positive?).to be true }
+  specify { expect(Player.all.count.positive?).to be true }
+  specify { expect(Team.all.count.positive?).to be true }
 
   describe 'as a user' do
     describe 'when I visit the competition index' do
