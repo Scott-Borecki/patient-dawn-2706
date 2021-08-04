f87b541e5fcacf01fbc26d417a90882cc2312fb3
Scott Borecki <scottborecki@gmail.com>
2021-08-04 11:55:55 -0600

    Refactor with before_action

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,21 +1,25 @@
 class ParticipantsController < ApplicationController
+  before_action :fetch_current_competition, only: [:create, :new]
+
   def new
-    @competition = Competition.find(params[:competition_id])
     @participant = Participant.new(competition: @competition)
   end
 
   def create
-    competition = Competition.find(params[:competition_id])
     team = Team.create!(participant_params)
 
-    competition.participants.create!(team: team)
+    @competition.participants.create!(team: team)
 
-    redirect_to competition_path(competition)
+    redirect_to competition_path(@competition)
   end
 
   private
 
   def participant_params
     params.required(:participant).permit(:nickname, :hometown)
+  end
+
+  def fetch_current_competition
+    @competition = Competition.find(params[:competition_id])
   end
 end
