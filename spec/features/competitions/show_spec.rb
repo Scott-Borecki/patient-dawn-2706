f1762d7badb405da9536068a0b852fba8fb1d4b9
Scott Borecki <scottborecki@gmail.com>
2021-08-04 10:19:25 -0600

    Add team nickname and hometown to competition show page

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -25,14 +25,29 @@ RSpec.describe 'competitions show page (/competitions/:id)' do
     )
   end
 
+  let!(:team1) { Team.create!(hometown: 'Lessburg', nickname: 'Rockets') }
+  let!(:team2) { Team.create!(hometown: 'Sandwich', nickname: 'Blue Knights') }
+  let!(:team3) { Team.create!(hometown: 'Arvada', nickname: 'Mountaineers') }
+  let!(:team4) { Team.create!(hometown: 'Jefferson', nickname: 'Spectacles') }
+  let!(:team5) { Team.create!(hometown: 'Washington', nickname: 'Cherries') }
+
+  let!(:participant1) {Participant.create!(team: team1, competition: competition1) }
+  let!(:participant2) {Participant.create!(team: team2, competition: competition1) }
+  let!(:participant3) {Participant.create!(team: team3, competition: competition1) }
+  let!(:participant4) {Participant.create!(team: team4, competition: competition2) }
+  let!(:participant5) {Participant.create!(team: team5, competition: competition2) }
+  let!(:participant6) {Participant.create!(team: team1, competition: competition3) }
+  let!(:participant7) {Participant.create!(team: team4, competition: competition3) }
   specify { expect(Competition.all.count.positive?).to be true }
+  specify { expect(Team.all.count.positive?).to be true }
 
   describe 'as a user' do
     describe 'when I visit the competitions show page' do
-      it 'displays the competitions name, location, and sport' do
-        visit competition_path(competition1)
+      before { visit competition_path(competition1) }
 
-        expect(current_path).to eq(competition_path(competition1))
+      specify { expect(current_path).to eq(competition_path(competition1)) }
+
+      it 'displays the competitions name, location, and sport' do
         expect(page).to have_content(competition1.name)
         expect(page).to have_content(competition1.location)
         expect(page).to have_content(competition1.sport)
@@ -46,9 +61,29 @@ RSpec.describe 'competitions show page (/competitions/:id)' do
         expect(page).to have_no_content(competition3.sport)
       end
 
-      it 'displays the nickname and hometown of all the teams in this competition'
+      context 'when a team is in the competition' do
+        it 'displays the nickname and hometown of the team' do
+          teams_in_competition = competition1.teams
 
-      it 'displays the average age of all players in the competition'
+          teams_in_competition.each do |team|
+            within "#team-#{team.id}" do
+              expect(page).to have_content(team.nickname)
+              expect(page).to have_content(team.hometown)
+            end
+          end
+        end
+      end
+
+      context 'when a team is not in the competition' do
+        it 'does not display the nickname and hometown of the team' do
+          teams_not_in_competition = [team4, team5]
+
+          teams_not_in_competition.each do |team|
+            expect(page).to have_no_content(team.nickname)
+            expect(page).to have_no_content(team.hometown)
+          end
+        end
+      end
     end
   end
 end
